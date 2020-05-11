c1d1ab0cb89bc0d4eb50804ff40286221884872d
Ronald <ronald.lou88@gmail.com>
2020-05-11 17:27:12 -0500

    add: models :octocat:

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
+import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Location Detail'),
+        title: Text(location.name),
       ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ImageBanner("assets/images/kiyomizu-dera.jpg"),
-            TextSection(Colors.red),
-            TextSection(Colors.green),
-            TextSection(Colors.blue),
-          ]),
+            ImageBanner(location.imagePath),
+          ]..addAll(textSections(location))),
     );
+  }
+
+  List<Widget> textSections(Location location) {
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList();
   }
 }
